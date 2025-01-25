18579cccb0da03c340c2fadfec42878f83459bd8
Roman Mishchuk <romamiykg@gmail.com>
2025-01-25 02:35:58 +0200

    Moved GUI variables into a separate class

--- gui.py
+++ gui.py
@@ -1,10 +1,4 @@
-# https://github.com/pyimgui/pyimgui/blob/master/doc/examples/plots.py
-
-import sys
-import pathlib
-from typing import Optional
-from time import sleep
-import json
+# Src: https://github.com/pyimgui/pyimgui/blob/master/doc/examples/plots.py
 
 import PIL.Image
 import PIL.ImageFile
@@ -16,7 +10,13 @@ import PIL
 from PIL import Image
 import numpy as np
 
+import sys
+import pathlib
+from typing import Optional
+from time import sleep
+import json
 from collections.abc import Callable
+from dataclasses import dataclass
 
 import algo.board
 from algo.board import Board
@@ -69,31 +69,47 @@ class ImageTexture:
 			c_id = (gl.GLuint * 1) (self.texture_id)
 			gl.glDeleteTextures(1, c_id)
 
-def on_pressed_tile(board: Board, pos: tuple[int, int]) -> None:
-	global selected_pos
+class UIState:
+	def __init__(self) -> None:
+		self.show_settings: bool = True
+		self.textures: dict[int, ImageTexture] = {
+			-2: ImageTexture("./icons/negative_king.png"),
+			-1: ImageTexture("./icons/negative_simple.png"),
+			0: ImageTexture("./icons/empty.png"),
+			1: ImageTexture("./icons/positive_simple.png"),
+			2: ImageTexture("./icons/positive_king.png"),
+		}
 
-	if selected_pos and pos in selected_pos[1]:
-		_move_result = board.user_move(selected_pos[0], pos)
-		selected_pos = None
-		return
+		self.selected_pos: Optional[tuple[
+			tuple[int, int],
+			set[tuple[int, int]]
+		]] = None
 
-	if not board.is_valid_pos(pos) or not board[pos] or \
-			(selected_pos and pos == selected_pos[0]) or \
-			algo.board._s(board[pos]) != board.turn_sign or \
-			not board.get_correct_moves(pos):
-		selected_pos = None
-		return
+		self.board: Board = Board()
 
-	selected_pos = (
-		pos,
-		board.get_correct_moves(pos)
-	)
+	def on_pressed_tile(self, pos: tuple[int, int]) -> None:
+		if self.selected_pos and pos in self.selected_pos[1]:
+			_move_result = self.board.user_move(self.selected_pos[0], pos)
+			self.selected_pos = None
+			return
+
+		if not self.board.is_valid_pos(pos) or not self.board[pos] or \
+				(self.selected_pos and pos == self.selected_pos[0]) or \
+				algo.board._s(self.board[pos]) != self.board.turn_sign or \
+				not self.board.get_correct_moves(pos):
+			self.selected_pos = None
+			return
+
+		self.selected_pos = (
+			pos,
+			self.board.get_correct_moves(pos)
+		)
 		
 
-def draw_board(board: Board, pos: tuple[float, float], available_size: tuple[float, float], gap_portion: float = 0.07) -> None:
+def draw_board(state: UIState, pos: tuple[float, float], available_size: tuple[float, float], gap_portion: float = 0.07) -> None:
 	used_size = min(available_size)
 	d = 10
-	size = (used_size - (board.SIZE + 1) * d) / (board.SIZE * (1 + gap_portion) + gap_portion)
+	size = (used_size - (state.board.SIZE + 1) * d) / (state.board.SIZE * (1 + gap_portion) + gap_portion)
 	gap = size * gap_portion + d
 	x_s = pos[0] + gap
 	x_c = x_s
@@ -102,25 +118,25 @@ def draw_board(board: Board, pos: tuple[float, float], available_size: tuple[flo
 	for y in range(6):
 		for x in range(6):
 			pos = (x, y)
-			piece = board[pos]
+			piece = state.board[pos]
 			sign = algo.board._s(piece)
 			imgui.set_cursor_pos_x(x_c)
 			imgui.set_cursor_pos_y(y_c)
 
 			# Highlighting valid moves with nice green
-			if selected_pos and pos in selected_pos[1]:
+			if state.selected_pos and pos in state.selected_pos[1]:
 				imgui.push_style_color(imgui.COLOR_BUTTON, 0.0, 0.5, 0.0, 1.0)
 				imgui.push_style_color(imgui.COLOR_BUTTON_HOVERED, 0.0, 0.5, 0.0, 0.6)
 				imgui.push_style_color(imgui.COLOR_BUTTON_ACTIVE, 0.0, 0.5, 0.0, 1.0)
 			
 			# Highlighting selected piece with nice yellow
-			elif selected_pos and pos == selected_pos[0]:
+			elif state.selected_pos and pos == state.selected_pos[0]:
 				imgui.push_style_color(imgui.COLOR_BUTTON, 0.5, 0.5, 0.0, 1.0)
 				imgui.push_style_color(imgui.COLOR_BUTTON_HOVERED, 0.5, 0.5, 0.0, 0.6)
 				imgui.push_style_color(imgui.COLOR_BUTTON_ACTIVE, 0.5, 0.5, 0.0, 1.0)
 			
 			# Dark tiles (which are valid)
-			elif board.is_valid_pos(pos):
+			elif state.board.is_valid_pos(pos):
 				imgui.push_style_color(imgui.COLOR_BUTTON, 0.55, 0.27, 0.07, 1.0)  # Dark brown
 				imgui.push_style_color(imgui.COLOR_BUTTON_HOVERED, 0.55, 0.27, 0.07, 0.6)
 				imgui.push_style_color(imgui.COLOR_BUTTON_ACTIVE, 0.55, 0.27, 0.07, 1.0)
@@ -132,28 +148,28 @@ def draw_board(board: Board, pos: tuple[float, float], available_size: tuple[flo
 				imgui.push_style_color(imgui.COLOR_BUTTON_ACTIVE, 0.87, 0.72, 0.53, 1.0)
 
 			if piece == 0:
-				imgui.image_button(textures[0].texture_id, size - 2, size)
+				imgui.image_button(state.textures[0].texture_id, size - 2, size)
 			else:
-				if board.get_correct_moves(pos) and sign == board.turn_sign:
-					imgui.image_button(textures[piece].texture_id, size - 2, size)
+				if state.board.get_correct_moves(pos) and sign == state.board.turn_sign:
+					imgui.image_button(state.textures[piece].texture_id, size - 2, size)
 				else:
 					# imgui.push_style_var(imgui.STYLE_ALPHA, 0.5)
 					# imgui.image_button(textures[piece].texture_id, size - 2, size)
 					# imgui.pop_style_var(1)
 
-					imgui.image_button(textures[piece].disabled_texture_id, size - 2, size)
+					imgui.image_button(state.textures[piece].disabled_texture_id, size - 2, size)
 			
 			imgui.pop_style_color(3)
 
 			if imgui.is_item_hovered():
 				if imgui.is_mouse_clicked():
-					on_pressed_tile(board, pos)
+					state.on_pressed_tile(pos)
 
 				with imgui.begin_tooltip():
 					imgui.text(f"pos: {pos}")
 					imgui.text(f"piece: {piece}")
-					if selected_pos and selected_pos[0] != pos:
-						imgui.text(f"Correct move: {board.is_move_correct(selected_pos[0], pos)}")
+					if state.selected_pos and state.selected_pos[0] != pos:
+						imgui.text(f"Correct move: {state.board.is_move_correct(state.selected_pos[0], pos)}")
 
 			x_c += size + gap
 
@@ -161,33 +177,14 @@ def draw_board(board: Board, pos: tuple[float, float], available_size: tuple[flo
 		x_c = x_s
 
 
-selected_pos: Optional[tuple[
-	tuple[int, int],
-	set[tuple[int, int]]
-]]
-
 def main():
 	window = __impl_glfw_init()
 	imgui.create_context()
 	impl = GlfwRenderer(window)
 
 	# INIT STAGE
-	show_settings = True
+	state = UIState()
 	imgui.get_io().font_global_scale = 1.5
-
-	global textures
-	textures = {
-		-2: ImageTexture("./icons/negative_king.png"),
-		-1: ImageTexture("./icons/negative_simple.png"),
-		0: ImageTexture("./icons/empty.png"),
-		1: ImageTexture("./icons/positive_simple.png"),
-		2: ImageTexture("./icons/positive_king.png"),
-	}
-
-	global selected_pos
-	selected_pos = None
-
-	board = Board()
 
 	# MAIN LOOP
 	while not glfw.window_should_close(window):
@@ -196,7 +193,7 @@ def main():
 
 		imgui.new_frame()
 
-		turn_name = "positive" if board.turn_sign > 0 else "negative"
+		turn_name = "positive" if state.board.turn_sign > 0 else "negative"
 
 		# Board window
 		# https://github.com/ocornut/imgui/issues/6872
@@ -204,7 +201,7 @@ def main():
 		imgui.set_window_size(500, 500)
 		imgui.set_window_position(10, 50, imgui.ONCE)
 		draw_board(
-			board,
+			state,
 			imgui.get_cursor_pos(),
 			imgui.get_content_region_available()
 		)
@@ -214,33 +211,46 @@ def main():
 		with imgui.begin_main_menu_bar():
 			clicked, _ = imgui.menu_item("Properties", "Ctrl+,", False)
 			if clicked:
-				show_settings = True
+				state.show_settings = True
 
 		# Settings window
-		if show_settings:
-			_, show_settings = imgui.begin("Properties", True, imgui.WINDOW_NO_COLLAPSE)
+		if state.show_settings:
+			_, state.show_settings = imgui.begin("Properties", True, imgui.WINDOW_NO_COLLAPSE)
 			imgui.set_window_size(400, 500)
 			imgui.set_window_position(510, 50, imgui.APPEARING)
 
 			if imgui.button("Reset board"):
-				board = Board()
+				state.board = Board()
 
-			_, board.enable_update_should_capture = imgui.checkbox(
-				"Enable should capture rule", board.enable_update_should_capture)
+			_, state.board.enable_update_should_capture = imgui.checkbox(
+				"Enable should capture rule", state.board.enable_update_should_capture)
 
 			imgui.separator()
-			imgui.text(f"{board.game_state}, turn: {turn_name}")
+			imgui.text(f"{state.board.game_state}, turn: {turn_name}")
+
+			# Players selection
+			imgui.separator()
+			with imgui.begin_table("Players selection", 2):
+				imgui.table_next_row()
+				imgui.table_next_column()
+				imgui.text("Positive:")
+				imgui.table_next_column()
+				imgui.text("Negative:")
+
+				imgui.table_next_row()
+				imgui.table_next_column()
+				# imgui.com
 			
 			imgui.separator()
-			imgui.text(f"Board state:\n{board}")
+			imgui.text(f"Board state:\n{state.board}")
 
 			imgui.separator()
-			imgui.text(f"Should capture:\npositive: {board.check_should_capture(1)}\nnegative: {board.check_should_capture(-1)}")
+			imgui.text(f"Should capture:\npositive: {state.board.check_should_capture(1)}\nnegative: {state.board.check_should_capture(-1)}")
 
 			imgui.separator()
 			imgui.text("Correct moves cache:")
 			imgui.text(json.dumps(
-				{repr(k): v for (k,v) in board.get_correct_moves_cache().items()},
+				{repr(k): v for (k,v) in state.board.get_correct_moves_cache().items()},
 				indent=2
 			))
 			
@@ -259,6 +269,7 @@ def main():
 		glfw.swap_buffers(window)
 		sleep(10e-3)
 
+	del state
 	impl.shutdown()
 	glfw.terminate()
 
