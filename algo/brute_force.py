4f3104e995931483e3d3e4e42915d10a4f3bec28
Roman Mishchuk <romamiykg@gmail.com>
2025-01-27 03:50:56 +0200

    Parallel brute force

--- algo/brute_force.py
+++ algo/brute_force.py
@@ -1,32 +1,46 @@
-from board import Board, GameState
 import copy
+import concurrent.futures
+import pickle
 
-arr: set[bytes] = set()
-b = Board()
-q: list[tuple[bytes, Board]] = [(bytes(b), b)]
-prev_max = 0
+from board import Board, GameState
 
-while q:
-	cur_bytes, cur = q.pop()
-	arr.add(cur_bytes)
 
-	if cur.game_state != GameState.NOT_OVER:
-		continue
-
-	for start in cur.get_possible_pos():
-		for end in cur.get_correct_moves(start):
-			new_board = copy.deepcopy(cur)
+def process_board(board_int: int, board: Board) -> tuple[int, list[tuple[int, Board]]]:
+	possible_moves: list[tuple[int, Board]] = []
+	for start in board.get_possible_pos():
+		for end in board.get_correct_moves(start):
+			new_board = copy.deepcopy(board)
 			new_board.make_move(start, end)
-			new_board_bytes = bytes(new_board)
+			possible_moves.append((int(new_board), new_board))
+	
+	return board_int, possible_moves
 
-			if new_board_bytes in arr:
+if __name__ == "__main__":
+	executor = concurrent.futures.ProcessPoolExecutor(max_workers=8)
+	root_board = Board()
+	jobs = [executor.submit(process_board, int(root_board), root_board)]
+	parent_tree: dict[int, set[int]] = {}
+	prev_len = 0
+
+	while jobs:
+		last_job = jobs.pop()
+
+		parent_board_int, possible_moves = last_job.result()
+		for child_board_int, child_board in possible_moves:
+			if child_board_int in parent_tree:
+				parent_tree[child_board_int].add(parent_board_int)
 				continue
 
-			q.append((new_board_bytes, new_board))
-	
-	l = len(arr)
-	if l % 1e3 == 0 and l > prev_max:
-		print(l)
-		prev_max = l
+			parent_tree[child_board_int] = {parent_board_int}
 
-print(len(arr))
+			if child_board.game_state == GameState.NOT_OVER:
+				jobs.append(executor.submit(process_board, child_board_int, child_board))
+
+		l = len(parent_tree)
+		if l - prev_len > 5e4:
+			prev_len = l
+			print(len(parent_tree), l)
+			with open("parent_tree.pickle", "wb") as f:
+				pickle.dump(parent_tree, f)
+
+	print(len(parent_tree))
