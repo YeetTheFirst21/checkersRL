229e469b9431d0b473c06c0dcdf7e4ad4d378b85
Roman Mishchuk <romamiykg@gmail.com>
2025-01-28 00:56:23 +0200

    Testing dynamicProgramming model that is currently saved on the branch

--- algo/iplayer.py
+++ algo/iplayer.py
@@ -3,23 +3,29 @@ from abc import ABC, abstractmethod
 from typing import Callable
 from os import PathLike
 
-from .board import Board, _s
+from .board import Board, GameState
 
 class IPlayer(ABC):
 	@abstractmethod
 	def decide_move(self, board: Board) -> tuple[tuple[int, int], tuple[int, int]]:
 		pass
 
-	def do_training_round(self, enemy: 'IPlayer', sign: int) -> bool:
-		"""Returns true if the model won the game"""
-		return False
+	def do_round(self, enemy: 'IPlayer') -> bool:
+		board = Board()
+		while True:
+			if board.game_state != GameState.NOT_OVER:
+				return board.game_state == GameState.POSITIVE_WINS
+			s, e = self.decide_move(board)
+			board.make_move(s, e)
+
+			if board.game_state != GameState.NOT_OVER:
+				return board.game_state == GameState.POSITIVE_WINS
+			s, e = enemy.decide_move(board)
+			board.make_move(s, e)
 
 class UserInput(IPlayer):
 	def decide_move(self, board: Board) -> tuple[tuple[int, int], tuple[int, int]]:
 		raise NotImplementedError("Should be handled by UI side")
-	
-	def do_training_round(self, enemy: IPlayer, sign: int) -> bool:
-		raise NotImplementedError("Can't train user")
 
 	def __str__(self) -> str:
 		return "User input"
@@ -59,9 +65,6 @@ class RandomPlayer(IRandomPlayer):
 			return start, end
 		
 		raise ValueError("Tried to ask for a move when there are no possible moves")
-	
-	def do_training_round(self, enemy: IPlayer, sign: int) -> bool:
-		raise NotImplementedError("Can't train random player")
 
 	def __str__(self) -> str:
 		return "Random player"
@@ -78,4 +81,10 @@ class ITrainablePlayer(IPlayer):
 
 	@abstractmethod
 	def load_model(self, path: PathLike | str) -> None:
+		pass
+
+class IParallelTrainablePlayer(ITrainablePlayer):
+	@staticmethod
+	@abstractmethod
+	def join(models: list['IParallelTrainablePlayer']) -> 'IParallelTrainablePlayer':
 		pass
