69ed3f5d11c4e7a772a634d0862fc7b162d12ddb
Roman Mishchuk <romamiykg@gmail.com>
2025-01-27 19:44:03 +0200

    Brute forcing for weak kings

--- algo/brute_force.py
+++ algo/brute_force.py
@@ -1,11 +1,12 @@
 import copy
 import concurrent.futures
 import pickle
+import time
 
 from board import Board, GameState
 
 
-def process_board(board_int: int, board: Board) -> tuple[int, list[tuple[int, Board]]]:
+def process_board(board_int: int, board: Board, depth: int) -> tuple[int, list[tuple[int, Board]], int]:
 	possible_moves: list[tuple[int, Board]] = []
 	for start in board.get_possible_pos():
 		for end in board.get_correct_moves(start):
@@ -13,19 +14,21 @@ def process_board(board_int: int, board: Board) -> tuple[int, list[tuple[int, Bo
 			new_board.make_move(start, end)
 			possible_moves.append((int(new_board), new_board))
 	
-	return board_int, possible_moves
+	return board_int, possible_moves, depth
 
 if __name__ == "__main__":
 	executor = concurrent.futures.ProcessPoolExecutor(max_workers=8)
 	root_board = Board()
-	jobs = [executor.submit(process_board, int(root_board), root_board)]
+	jobs = [executor.submit(process_board, int(root_board), root_board, 0)]
 	parent_tree: dict[int, set[int]] = {}
+	depths: dict[int, int] = {}
 	prev_len = 0
 
 	while jobs:
 		last_job = jobs.pop()
 
-		parent_board_int, possible_moves = last_job.result()
+		parent_board_int, possible_moves, depth = last_job.result()
+		depths[depth] = depths.get(depth, 0) + 1
 		for child_board_int, child_board in possible_moves:
 			if child_board_int in parent_tree:
 				parent_tree[child_board_int].add(parent_board_int)
@@ -34,12 +37,12 @@ if __name__ == "__main__":
 			parent_tree[child_board_int] = {parent_board_int}
 
 			if child_board.game_state == GameState.NOT_OVER:
-				jobs.append(executor.submit(process_board, child_board_int, child_board))
+				jobs.append(executor.submit(process_board, child_board_int, child_board, depth + 1))
 
 		l = len(parent_tree)
 		if l - prev_len > 5e4:
 			prev_len = l
-			print(len(parent_tree), l)
+			print(len(parent_tree), len(jobs), time.strftime("%H:%M:%S"), depths)
 			with open("parent_tree.pickle", "wb") as f:
 				pickle.dump(parent_tree, f)
 
